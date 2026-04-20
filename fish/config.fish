4ce0f13e8024a439ee323d113baf18af7a9bcc05
real-hong <xiulieshan@gmail.com>
2026-04-20 22:37:37 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,11 @@
 set -x EDITOR nvim
-set -x PATH "$HOME/.local/bin" $PATH
-set -x PATH "$HOME/go/bin" $PATH
-set -x PATH "$HOME/.cargo/bin" $PATH
 set -x GTK_IM_MODULE fcitx
 set -x QT_IM_MODULE fcitx
 set -x XMODIFIERS "@im=fcitx"
+
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.cargo/bin"
+fish_add_path "$HOME/go/bin"
 
 alias nv=nvim
 alias cat=bat
